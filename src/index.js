129be04e17cd835bf835ecf46f67848288d8fa77
Gabriel Gambarra <gambarragabriel@gmail.com>
2019-05-06 03:25:59 -0400

    Forgot Password

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ app.get('/', (req, res) => {
     res.send('OK');
 });
 
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
+require('./app/controllers/index')(app);
 
 
 app.listen(3000);
