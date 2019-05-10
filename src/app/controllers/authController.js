68c7e76cf75d802babad3a34f12883cf3f50ce26
Gabriel Gambarra <gabriel.gambarra@iatecam.org.br>
2019-05-10 15:01:41 -0400

    Tratamento de CORS

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -40,6 +40,10 @@ router.post('/register', async (req, res) => {
 router.post('/authenticate', async (req, res) => {
     const { email, password } = req.body;
 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
+
     const user = await User.findOne({ email }).select('+password');
 
     if (!user) {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -12,4 +14,4 @@ app.get('/', (req, res) => {
 
 require('./app/controllers/index')(app);
 
-app.listen(3000);
+app.listen(5000);
